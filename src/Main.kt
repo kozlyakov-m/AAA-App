6d93a05ff54e83838e772078209e143732c9bdb4
Max <kozlyakov.m@gmail.com>
2020-03-20 19:36:05 +0700

    ROADMAP 4.i
    Создать метод нужна ли аутентификация

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,14 @@ fun main(args: Array<String>) {
         businessLogic.printHelp()
         exitProcess(1)
     } else {
-        if (args[0] == "-h"){
+        if (args[0] == "-h") {
             businessLogic.printHelp()
             exitProcess(1)
         }
     }
     exitProcess(0)
+}
+
+fun isAuthenticationNeeded(args: Array<String>): Boolean {
+    return args[0]=="-login" && args[2]=="-pass"
 }
