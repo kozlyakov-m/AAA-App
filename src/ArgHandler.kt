67b3579659cf298445667f8e520efec09c959421
Max <kozlyakov.m@gmail.com>
2020-03-21 15:29:48 +0700

    Fix ArgHandler

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -1,43 +1,38 @@
 import kotlin.system.exitProcess
 
-class ArgHandler(val args: Array<String>, val businessLogic: BusinessLogic) {
-
-    var login: String = ""
-    var pass: String = ""
+class ArgHandler(val args: Array<String) {
+	
+	
+    var login: String? = null
+    var pass: String? = null
 
     init {
-        if (isHelpNeeded()) {
-            businessLogic.printHelp()
-            exitProcess(1)
-        } else if (isAuthenticationNeeded()) {
-            login = args[1]
-            pass = args[3]
-            // if login or pass is not correct program will exit
-            businessLogic.authentication(login, pass)
-        } else if (isAutorizationNeeded()) {
-            // check that autorization neccessary, if not - exit
-        } else {
-
-        }
+		
+		if(isHelpNeeded){
+			exitProcess(1) //надо возвращать в Main
+		}
+		
+		if(args.size >= 4){
+			login = args[1]
+			pass = args[3]
+		}
+		else{
+			exitProcess(1)
+		}
+		
+		//перед получением следующих параметров проверяем ещё раз длину массива 
+		
     }
 
     fun isHelpNeeded(): Boolean {
         if (args.isEmpty()) {
             return true
         } else if (args[0] == "-h") {
-            return true
+            return true	
         }
         return false
     }
 
-    fun isAuthenticationNeeded(): Boolean {
-        if (args[0] == "-login" && args[2] == "-pass" ) {
-            return true
-        } else {
-            businessLogic.printHelp()
-            exitProcess(0)
-        }
-    }
-
+	fun isAuthenticationNeeded(): Boolean = login != null && pass != null
 
 }
