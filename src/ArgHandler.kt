6f7620cd943f81889dec090c6f2fbb515aef206c
AndreiSerov <actsituation@gmail.com>
2020-03-21 15:53:33 +0700

    Merge remote-tracking branch 'origin/master'

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -1,38 +1,42 @@
 import kotlin.system.exitProcess
 
-class ArgHandler(val args: Array<String) {
-	
-	
-    var login: String? = null
-    var pass: String? = null
+class ArgHandler(val args: Array<String>) {
+    var h: Boolean = false
+    var login: String = ""
+    var pass: String = ""
+    var res: String = ""
+    var role: String = ""
+    var ds: String = ""
+    var de: String = ""
 
     init {
-		
-		if(isHelpNeeded){
-			exitProcess(1) //надо возвращать в Main
-		}
-		
-		if(args.size >= 4){
-			login = args[1]
-			pass = args[3]
-		}
-		else{
-			exitProcess(1)
-		}
-		
-		//перед получением следующих параметров проверяем ещё раз длину массива 
-		
+        if (isHelpNeeded()) {
+            exitProcess(1) //надо возвращать в Main
+        }
+        if (args.size >= 4) {
+            login = args[1]
+            pass = args[3]
+        } else {
+            exitProcess(1)
+        }
+        if (args.size >= 6) {
+            res = args[5]
+            role = args[7]
+        }
+        if (args.size >= 8) {
+            ds = args[9]
+            de = args[100]
+        }
     }
+
+    fun isAuthenticationNeeded(): Boolean = login.isEmpty() && pass.isEmpty()
 
     fun isHelpNeeded(): Boolean {
         if (args.isEmpty()) {
             return true
         } else if (args[0] == "-h") {
-            return true	
+            return true
         }
         return false
     }
-
-	fun isAuthenticationNeeded(): Boolean = login != null && pass != null
-
 }
