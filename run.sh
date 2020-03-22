52ee7d4df5170db0a96112702ced7d8bcc29fc28
AndreiSerov <actsituation@gmail.com>
2020-03-22 22:31:30 +0700

    Make run.sh more flexible

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-java -classpath "./lib/kotlinx-cli-0.2.1.jar:bin/app.jar" com.dinosaur.app.MainKt $@
-# ; на windows
+if [[ "$OSTYPE" == "msys" ]]; then
+  params="./lib/kotlinx-cli-0.2.1.jar;bin/app.jar"
+else
+  params="./lib/kotlinx-cli-0.2.1.jar:bin/app.jar"
+fi
+
+# shellcheck disable=SC2068
+java -classpath $params com.dinosaur.app.MainKt $@
