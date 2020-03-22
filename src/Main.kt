7eb6ad23c88add25143a1ca112e21dd375c4d6dd
Max <kozlyakov.m@gmail.com>
2020-03-22 17:27:41 +0700

    Close #32

--- src/Main.kt
+++ src/Main.kt
@@ -43,26 +43,26 @@ fun main(args: Array<String>) {
     val argHandler = ArgHandler(args)
     val businessLogic = BusinessLogic()
 
-    val user: User = (
+    val user: User =
             if (argHandler.isAuthenticationRequired()) {
                 businessLogic.authentication(argHandler.login!!, argHandler.pass!!)
             } else {
                 exitProcess(1)
-            })
+            }
 
-    val permission: Permission = (
+    val permission: Permission =
             if (argHandler.isAuthorizationRequired()) {
                 businessLogic.authorization(argHandler.res, argHandler.role, user.login) ?: exitProcess(6)
             } else {
                 exitProcess(0) //0 так как аутентификация прошла успешно
-            })
+            }
 
-    val session: Session = (
+    val session: Session =
             if (argHandler.isAccountingRequired()) {
                 businessLogic.accounting(permission, argHandler.ds, argHandler.de, argHandler.vol) ?: exitProcess(7)
             } else {
                 exitProcess(0)
-            })
+            }
 
     sessions.add(session)
     exitProcess(0)
