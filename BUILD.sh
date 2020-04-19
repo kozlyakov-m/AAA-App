3575a9dafbe13b9aea21034f26445133d2609966
Max <kozlyakov.m@gmail.com>
2020-04-19 23:07:26 +0700

    Update build.sh and run.sh

--- BUILD.sh
+++ BUILD.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-mkdir -p bin
-kotlinc -classpath "./lib/kotlinx-cli-0.2.1.jar;lib/h2-1.4.200.jar" ./src -include-runtime -d bin/app.jar
+./gradlew shadowJar
--- RUN.sh
+++ RUN.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "msys" ]]; then
-  params="./lib/kotlinx-cli-0.2.1.jar;lib/h2-1.4.200.jar;bin/app.jar"
-else
-  params="./lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar:bin/app.jar"
-fi
 
-# shellcheck disable=SC2068
-java -classpath $params com.dinosaur.app.MainKt $@
+java -jar AAA-App.jar $@
