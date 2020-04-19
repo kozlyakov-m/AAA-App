dd578b3c640bb6bd078fa5abb447cb1f7b5b36e0
Max <kozlyakov.m@gmail.com>
2020-04-19 23:35:32 +0700

    Create AuthenticationDAO

--- src/main/kotlin/com/project/app/Main.kt
+++ src/main/kotlin/com/project/app/Main.kt
@@ -1,27 +1,20 @@
 package com.project.app
 
+import com.project.app.dao.AuthenticationDAO
 import com.project.app.domain.Permission
 import com.project.app.service.AccountingService
 import com.project.app.service.AuthenticationService
 import com.project.app.service.AuthorizationService
-import java.sql.*
+
 import kotlin.system.exitProcess
 import org.flywaydb.core.Flyway
+import java.sql.Connection
+import java.sql.DriverManager
 
 fun main(args: Array<String>) {
 
     /*val conn: Connection = DriverManager.
             getConnection("jdbc:h2:./test", "sa", "");
-
-    val q = "CREATE TABLE users (id int, login varchar(255))"
-    val q1 = "INSERT INTO users VALUES(1, 'name1')"
-    val q2 = "SELECT * FROM users"
-    val stmt = conn.createStatement()
-//    val res = stmt.execute(q)
-//    val res1 = stmt.execute(q1)
-    val res2 = stmt.executeQuery(q2)
-    res2.next()
-    println(res2.getString("id"))
     conn.close()*/
 
     val flyway = Flyway.configure()
@@ -30,44 +23,51 @@ fun main(args: Array<String>) {
             .load()
     flyway.migrate()
 
-    val argHandler = ArgHandler(args)
-    val authenticationService = AuthenticationService(users)
+    val dbConnection: Connection =  DriverManager.
+            getConnection("jdbc:h2:./AAA-App", "sa", "")
 
-    var exitCode: ExitCodes = if (argHandler.isAuthenticationRequired()) {
-        authenticationService.authentication(argHandler.login!!, argHandler.pass!!)
-    } else {
-        ExitCodes.HELP
+    dbConnection.use {
+        val argHandler = ArgHandler(args)
+
+        val authenticationDAO = AuthenticationDAO(dbConnection)
+        val authenticationService = AuthenticationService(authenticationDAO)
+
+        var exitCode: ExitCodes = if (argHandler.isAuthenticationRequired()) {
+            authenticationService.authentication(argHandler.login!!, argHandler.pass!!)
+        } else {
+            ExitCodes.HELP
+        }
+        if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
+
+        val user = authenticationService.user!! // если user null, программа завершится раньше
+
+        // if authentication passed create instance of AuthorizationService
+        val authorizationService = AuthorizationService(permissions)
+        exitCode = if (argHandler.isAuthorizationRequired()) {
+            authorizationService.authorization(argHandler.res, argHandler.role, user.login)
+        } else {
+            exitProcess(ExitCodes.SUCCESS.code) // 0 так как аутентификация прошла успешно
+        }
+        if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
+
+        val permission: Permission = authorizationService.permission!!
+
+        // if authorization passed create instance of AccountingService
+        val accountingService = AccountingService(sessions)
+        exitCode = if (argHandler.isAccountingRequired()) {
+            accountingService.accounting(
+                    permission,
+                    argHandler.ds,
+                    argHandler.de,
+                    argHandler.vol
+            )
+        } else {
+            exitProcess(ExitCodes.SUCCESS.code)
+        }
+
+        if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
+
+        sessions.add(accountingService.session!!)
+        exitProcess(exitCode.code)
     }
-    if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
-
-    val user = authenticationService.user!! // если user null, программа завершится раньше
-
-    // if authentication passed create instance of AuthorizationService
-    val authorizationService = AuthorizationService(permissions)
-    exitCode = if (argHandler.isAuthorizationRequired()) {
-        authorizationService.authorization(argHandler.res, argHandler.role, user.login)
-    } else {
-        exitProcess(ExitCodes.SUCCESS.code) // 0 так как аутентификация прошла успешно
-    }
-    if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
-
-    val permission: Permission = authorizationService.permission!!
-
-    // if authorization passed create instance of AccountingService
-    val accountingService = AccountingService(sessions)
-    exitCode = if (argHandler.isAccountingRequired()) {
-        accountingService.accounting(
-                permission,
-                argHandler.ds,
-                argHandler.de,
-                argHandler.vol
-        )
-    } else {
-        exitProcess(ExitCodes.SUCCESS.code)
-    }
-
-    if (exitCode != ExitCodes.SUCCESS) exitProcess(exitCode.code)
-
-    sessions.add(accountingService.session!!)
-    exitProcess(exitCode.code)
 }
--- src/main/kotlin/com/project/app/service/AuthenticationService.kt
+++ src/main/kotlin/com/project/app/service/AuthenticationService.kt
@@ -1,10 +1,11 @@
 package com.project.app.service
 
 import com.project.app.ExitCodes
+import com.project.app.dao.AuthenticationDAO
 import com.project.app.domain.User
 import java.security.MessageDigest
 
-class AuthenticationService(private val users: List<User>) {
+class AuthenticationService(val authenticationDAO: AuthenticationDAO) {
 
     var user: User? = null
 
@@ -13,7 +14,7 @@ class AuthenticationService(private val users: List<User>) {
             return ExitCodes.INVALID_LOGIN
         }
         // check that user exists in db
-        user = findUser(login, users) ?: return ExitCodes.USER_NOT_FOUND
+        user = authenticationDAO.findUser(login) ?: return ExitCodes.USER_NOT_FOUND
 
         if (!checkPassword(pass, user!!)) {
             return ExitCodes.WRONG_PASSWORD
@@ -23,23 +24,24 @@ class AuthenticationService(private val users: List<User>) {
 
     private fun isLoginValid(login: String): Boolean = "^[a-z]{1,10}$".toRegex().matches(login)
 
-    private fun findUser(login: String, users: List<User>): User? {
+/*    private fun findUser(login: String, users: List<User>): User? {
         for (user in users) {
             if (user.login == login) {
                 return user
             }
         }
         return null
-    }
+    }*/
 
     private fun checkPassword(pass: String, user: User) = pass.getHash(user.salt) == user.hash
 
     // stackoverflow driven development
     private fun String.getHash(
         salt: String,
-        algorithm: String = "SHA-512"
+        algorithm: String = "SHA-256"
     ): String {
-        val bytes = (salt + this).toByteArray()
+        //val bytes = (salt + this).toByteArray()
+        val bytes = (this + salt).toByteArray()
         val md = MessageDigest.getInstance(algorithm)
         val digest = md.digest(bytes)
         return digest.fold("", { str, it -> str + "%02x".format(it) })
